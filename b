27e9c446a84b4d1f939e417efe9b69b376f2b243
ZIRADDIN KAZIMLI <93098526+kziraddin@users.noreply.github.com>
2022-11-30 23:30:54 -0500

    Add files via upload

---
+++ b
@@ -0,0 +1,6 @@
+from search import *
+from notebook import psource, heatmap, gaussian_kernel, show_map, final_path_colors, display_visual, plot_NQueens
+
+# Needed to hide warnings in the matplotlib sections
+import warnings
+warnings.filterwarnings("ignore")
---
+++ test1.py
@@ -0,0 +1,19 @@
+"""from search import *
+from notebook import psource, heatmap, gaussian_kernel, show_map, final_path_colors, display_visual, plot_NQueens
+
+# Needed to hide warnings in the matplotlib sections
+import warnings
+warnings.filterwarnings("ignore")
+"""
+
+
+
+#matplotlib inline
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib import lines
+
+from ipywidgets import interact
+import ipywidgets as widgets
+from IPython.display import display
+import time
